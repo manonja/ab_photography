84d52bfc81a5038009172aee8110d18e46656493
manonja <manon.jacquin@gmail.com>
2024-01-03 10:12:52 +0100

    feat: add more photos to 7rad
    Former-commit-id: 1ec10d427b77d7e71decae7c139f9dbf681697db [formerly 3a695429045631f0deee0b540587ede6f8db2e7f]
    Former-commit-id: 44ba46be99efadc5ff731e55a3369ec5054443ee

--- app/7rad/page.tsx
+++ app/7rad/page.tsx
@@ -1,12 +1,23 @@
 import {Photo} from "../components/photo";
-import rad1 from "../../public/7rad/7rad1.jpg";
+import rad1 from "../../public/7rad/abossenbroek-7rad-00008.webp";
+import rad2 from "../../public/7rad/abossenbroek-7rad-00001.webp";
+import rad3 from "../../public/7rad/abossenbroek-7rad-00002.webp";
+import rad4 from "../../public/7rad/abossenbroek-7rad-00003.webp";
+import rad5 from "../../public/7rad/abossenbroek-7rad-00005.webp";
+import rad6 from "../../public/7rad/abossenbroek-7rad-00006.webp";
+import rad7 from "../../public/7rad/abossenbroek-7rad-00007.webp";
+import rad8 from "../../public/7rad/abossenbroek-7rad-00004.webp";
+
 import {Paragraph} from "../components/paragraph";
+import { Gallery } from "../components/gallery";
 
 export default function SevenRad() {
   const text = "The Netherlands has the highest level of artificial light at night per square kilometer of any OECD country. In 2020, satellites measured an average radiance (rad) in the Netherlands of 7 rad with a maximum of 10,596 rad. In perspective, New York state, USA has an average radiance of 2 rad and a maximum of 411 rad.Artificial light at night affects both humans and animals. Our melatonine hormone levels change, increasing the risk of cancer. As for animals, evolution made them trust the brightness of the day to determine the time of the day. Now, with bright skies at night, they are blinded.Through this work Anton Bossenbroek investigates the surreal landscapes that artificial light at night produces."
-  return (
+  const images = [rad1, rad6, rad3, rad8, rad5, rad2, rad7, rad4]
+;  return (
       <>
         <Photo photo={rad1} withDivider/>
+        <Gallery images={images}/>
         <Paragraph text={text}/>
       </>
   )
