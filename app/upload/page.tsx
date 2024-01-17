8c1219c73d668b855d3d606715e5ed9f51c03cba
manonja <manon.jacquin@gmail.com>
2024-01-17 16:54:46 +0100

    feat: add page to upload images to vercel blob storage

---
+++ app/upload/page.tsx
@@ -0,0 +1,14 @@
+import { UploadImages } from "../components/uploadImages";
+
+export default function Upload() {
+  return (
+      <div className="container pt-44">
+        <UploadImages/>
+      </div>
+      
+  )
+}
+
+
+
+
