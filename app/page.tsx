5fce0435599c1c181b18a4d97673705dbde607f1
manonja <manon.jacquin@gmail.com>
2024-02-02 19:26:34 +0100

    feat: test api call in server component with fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
-import {SlideShow} from "./components/slideShow";
+import { SlideShow } from "./components/slideShow";
 import { Photo } from "./types/photo";
 
+async function getPhotos() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api`, {
+    method: "GET",
+  });
 
-// async function getPhotos() {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api`, { method: "GET" });
+  const photos = await res.json();
 
-//   const photos = await res.json();
-
-//   return photos;
-// }
-
+  return photos;
+}
 
 export default async function Home() {
-  // const photos = await getPhotos();
+  const photos = await getPhotos();
 
-  // const slidesImages = photos.map((photo: Photo) => photo.desktop_blob);
+  const slidesImages = photos.map((photo: Photo) => photo.desktop_blob);
 
-  return <div>Work In Progress</div>;
+  return <SlideShow images={slidesImages} />;
 }
