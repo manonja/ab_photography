9843417f07ea9e08670cfe77fca6c618b436c945
Manon <manon.jacquin@gmail.com>
2024-02-24 17:37:45 +0100

    Feat: copy updates (#15)

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
-import { SlideShow } from "./components/slideShow";
-import { Photo } from "./types/photo";
+import {SlideShow} from "./components/slideShow";
+import {Photo} from "./types/photo";
 
 async function getPhotos() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api`, {
     method: "GET",
   });
 
-  const photos = await res.json();
-
-  return photos;
+  return await res.json();
 }
 
 export default async function Home() {
