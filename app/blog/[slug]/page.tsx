2cf420ff4e15a85013d408bc5ace8c52b6ad4ed6
manonja <manon.jacquin@gmail.com>
2024-02-28 17:37:21 +0100

    Improve readability

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { useMDXComponent } from "next-contentlayer/hooks";
-import { allPosts, Post } from "contentlayer/generated";
+import { allPosts } from "contentlayer/generated";
 import mdxComponents from "../../components/mdxComponents";
 
 const MDXPost: FC<{ slug: string }> = ({ slug }) => {
@@ -17,18 +17,14 @@ const MDXPost: FC<{ slug: string }> = ({ slug }) => {
   }
 
   return (
-    <>
+    <div className="flex flex-col mx-auto max-w-2xl items-center sm:pt-32 ">
       <h1 className="sm:py-8 py-4">{blogPost.title}</h1>
       <MdxContent components={mdxComponents} />
-    </>
+    </div>
   );
 };
 const BlogPage = ({ params }: { params: { slug: string } }) => {
-  return (
-    <div className="flex flex-col mx-auto max-w-2xl items-center sm:pt-32 ">
-      <MDXPost slug={params.slug} />
-    </div>
-  );
+  return <MDXPost slug={params.slug} />;
 };
 
 export default BlogPage;
