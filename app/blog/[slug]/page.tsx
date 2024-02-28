872f5bb129f8fde574ef231cb2d4d8a08f1bc93b
manonja <manon.jacquin@gmail.com>
2024-02-28 17:35:18 +0100

    Fix bad call hook

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,39 +1,34 @@
 import React, { FC } from "react";
-import {useMDXComponent} from "next-contentlayer/hooks";
-import {allPosts, Post} from "contentlayer/generated";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import { allPosts, Post } from "contentlayer/generated";
 import mdxComponents from "../../components/mdxComponents";
 
+const MDXPost: FC<{ slug: string }> = ({ slug }) => {
+  const blogPost = allPosts.find((post) => post?.slug === slug);
+  const MdxContent = useMDXComponent(blogPost?.body.code || "");
 
-const MDXPost: FC<{slug: string}> = ({slug}) => {
-    const blogPost = allPosts.find((post) => post?.slug === slug)
-
-    if (!blogPost) {
-        return (
-           <div className = "flex flex-col mx-auto max-w-2xl items-center sm:pt-32 " >
-                <h1 className="sm:py-8 py-4">Article not found</h1>
-                <div>Article with address {slug} does not seem to exist</div>
-            </div>
-
-        )
-    }
-    const MdxContent = useMDXComponent(blogPost.body.code)
-
+  if (!blogPost) {
     return (
+      <div className="flex flex-col mx-auto max-w-2xl items-center sm:pt-32 ">
+        <h1 className="sm:py-8 py-4">Article not found</h1>
+        <div>Article with address {slug} does not seem to exist</div>
+      </div>
+    );
+  }
 
-        <>
-            <h1 className="sm:py-8 py-4">{blogPost.title}</h1>
-            <MdxContent components={mdxComponents}/>
-
-        </>
-)
-}
-const BlogPage = ({params}: { params: { slug: string } }) => {
-    return (
-        <div className="flex flex-col mx-auto max-w-2xl items-center sm:pt-32 ">
-            <MDXPost slug={params.slug}/>
-        </div>
-    )
-
+  return (
+    <>
+      <h1 className="sm:py-8 py-4">{blogPost.title}</h1>
+      <MdxContent components={mdxComponents} />
+    </>
+  );
+};
+const BlogPage = ({ params }: { params: { slug: string } }) => {
+  return (
+    <div className="flex flex-col mx-auto max-w-2xl items-center sm:pt-32 ">
+      <MDXPost slug={params.slug} />
+    </div>
+  );
 };
 
-export default BlogPage
+export default BlogPage;
