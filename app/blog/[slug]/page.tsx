70b1238df4c224b8a8fc85e35e51168dbd647f0d
manonja <manon.jacquin@gmail.com>
2024-02-28 16:43:21 +0100

    feat: add custom mdx component example

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,25 +1,28 @@
 import React, { FC } from "react";
 import {useMDXComponent} from "next-contentlayer/hooks";
 import {allPosts, Post} from "contentlayer/generated";
+import mdxComponents from "../../components/mdxComponents";
 
 
 const MDXPost: FC<{slug: string}> = ({slug}) => {
     const blogPost = allPosts.find((post) => post?.slug === slug)
+
     if (!blogPost) {
         return (
-            <>
-                <div>Post not found</div>
-                <div>Page with address {slug} not found</div>
-            </>
+           <div className = "flex flex-col mx-auto max-w-2xl items-center sm:pt-32 " >
+                <h1 className="sm:py-8 py-4">Article not found</h1>
+                <div>Article with address {slug} does not seem to exist</div>
+            </div>
 
         )
     }
     const MdxContent = useMDXComponent(blogPost.body.code)
+
     return (
 
         <>
             <h1 className="sm:py-8 py-4">{blogPost.title}</h1>
-            <MdxContent/>
+            <MdxContent components={mdxComponents}/>
 
         </>
 )
