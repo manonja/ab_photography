e9a9a08d89435bef9a96fe0c00e3a89ffa9fd3d3
manonja <manon.jacquin@gmail.com>
2024-03-28 15:26:29 +0100

    feat: make date dynamic for copyright + tailwind grid improvement

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
-import {SubscribeForm} from "./subscribeForm";
+import { SubscribeForm } from "./subscribeForm";
 
 export const Footer = () => {
+  const thisYear = new Date().getFullYear();
+
   return (
     <footer className="bg-neutral-900 w-full h-24">
       <div className="flex flex-col items-start justify-center sm:flex-row sm:items-center sm:justify-between h-full  px-4 2xl:px-16 sm:mt-6">
         <div className="flex justify-between items-start w-full sm:w-auto">
-          <SubscribeForm/>
+          <SubscribeForm />
           <div className="sm:hidden flex items-center space-x-4">
             <div>
               <Link
@@ -25,7 +27,9 @@ export const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="text-xs text-neutral-600">© 2023 Anton Bossenbroek</div>
+        <div className="text-xs text-neutral-600">
+          © Anton Bossenbroek {thisYear}
+        </div>
         <div className="hidden sm:flex sm:space-x-4 sm:items-center">
           <div>
             <Link
--- app/components/imageGallery.tsx
+++ app/components/imageGallery.tsx
@@ -1,56 +1,56 @@
-'use client';
+"use client";
 
-import {FC, useState} from "react";
+import { FC, useState } from "react";
 import Image from "next/image";
 
 import FullScreenDialog from "./fullScreenDialog";
 import { Photo } from "../types/photo";
 
 interface ImageGalleryProps {
-    image: Photo;
-    images: Photo[]
+  image: Photo;
+  images: Photo[];
 }
 
 function cn(...classes: string[]) {
-    return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(" ");
 }
 
-export const ImageGallery:FC<ImageGalleryProps> = ({image, images}) =>  {
-    const [isLoading, setLoading] = useState(true);
-    const [isOpen, setIsOpen] = useState(false);
+export const ImageGallery: FC<ImageGalleryProps> = ({ image, images }) => {
+  const [isLoading, setLoading] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const openDialog = () => setIsOpen(true);
-    const closeDialog = () => setIsOpen(false);
- 
-    return (
-        <>
-                <div className="w-full bg-grey-900 overflow-hidden">
-                {/* eslint-disable-next-line react/jsx-no-undef */}
-                <Image
-                    alt={image.caption}
-                    src={image.gallery_blob}
-                    priority
-                    width={0}height={0}
-                    sizes="(max-width: 768px) 33vh, (max-width: 1200px) 50vw, 100vw"
-                    style={{ width: '100%', height: 'auto' }}
-                    className={cn(
-                        'group-hover:opacity-80 duration-700 ease-in-out object-cover cursor-pointer sm:max-h-48 sm:max-w-[100%] max-w-64',
-                        isLoading
-                            ? 'grayscale blur-2xl scale-110'
-                            : 'grayscale-0 blur-0 scale-100'
-                    )}
-                    onLoad={() => setLoading(false)}
-                    onClick={openDialog} 
-                />
-    
-            </div>
+  const openDialog = () => setIsOpen(true);
+  const closeDialog = () => setIsOpen(false);
 
-            <FullScreenDialog
-                images={images}
-                sequence={image.sequence}
-                isOpen={isOpen}
-                onClose={closeDialog}/>
-            </>
-    
-    );
-}
+  return (
+    <>
+      <div className="w-full bg-grey-900 overflow-hidden">
+        {/* eslint-disable-next-line react/jsx-no-undef */}
+        <Image
+          alt={image.caption}
+          src={image.gallery_blob}
+          priority
+          width={0}
+          height={0}
+          sizes="(max-width: 768px) 33vh, (max-width: 1200px) 50vw, 100vw"
+          style={{ width: "100%", height: "auto" }}
+          className={cn(
+            "group-hover:opacity-80 duration-700 ease-in-out object-cover cursor-pointer sm:max-h-48 sm:max-w-[100%] max-w-64",
+            isLoading
+              ? "grayscale blur-2xl scale-110"
+              : "grayscale-0 blur-0 scale-100"
+          )}
+          onLoad={() => setLoading(false)}
+          onClick={openDialog}
+        />
+      </div>
+
+      <FullScreenDialog
+        images={images}
+        sequence={image.sequence}
+        isOpen={isOpen}
+        onClose={closeDialog}
+      />
+    </>
+  );
+};
